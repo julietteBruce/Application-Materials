e5feea186c765bd0141c48b5137301160b68f2e9
Juliette Bruce <jemmy.bruce@gmail.com>
2026-03-04 12:26:07 -0600

    More stuff

---
+++ Confrences/MFO-AG2642/Leltter/auto/lettertemplate2019.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "lettertemplate2019"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "epsfig"
+    "longfbox"
+    "fullpage"
+    "url"
+    "datetime"
+    "lipsum"
+    "fancyhdr"
+    "fontspec"
+    "xcolor"))
+ :latex)
+
